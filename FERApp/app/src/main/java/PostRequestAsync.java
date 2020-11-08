44d629bacc6ebd36cbf028b568ef936d448e8048
notnamenot <izabela.pachel@wp.pl>
2020-11-08 17:30:26 +0100

    sending request

---
+++ FERApp/app/src/main/java/PostRequestAsync.java
@@ -0,0 +1,8 @@
+import android.os.AsyncTask;
+
+public class PostRequestAsync extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
--- FERApp/app/src/main/java/pl/edu/agh/studio2/ferapp/MainActivity.java
+++ FERApp/app/src/main/java/pl/edu/agh/studio2/ferapp/MainActivity.java
@@ -8,28 +8,66 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+//import java.util.Base64; //od API Level 26 (Android 8), a mamy 21 (Android 5)
+import org.apache.commons.codec.binary.Base64;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONString;
+
+//import okhttp3.MediaType;
+//import okhttp3.OkHttpClient;
+//import okhttp3.Request;
+//import okhttp3.RequestBody;
+//import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final String url = "https://ferappagh.azurewebsites.net/api/FER";
+//    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     TextView txt_random_emotion;
     Emotion random_emotion;
     Button btn_take_photo;
     Button btn_send_res;
     ImageView img_photo;
-    byte[] photo_byte_array;
+//    byte[] photo_byte_array;
+    String encodedPhoto;
+
+//    private OkHttpClient client;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        client = new OkHttpClient();
+        requestQueue = Volley.newRequestQueue(this);
 
         random_emotion = Emotion.randomEmotion();
 
@@ -51,14 +89,93 @@ public class MainActivity extends AppCompatActivity {
         btn_send_res.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                sendPhoto();
+                try {
+                    sendPhoto();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),"sendPhotoException", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
 
     }
 
-    private void sendPhoto() {
+    private void sendPhoto() throws JSONException {
+        JSONObject json = new JSONObject()
+                .put("id","randomid_jdfhg")
+                .put("image", encodedPhoto)
+                .put("emotionID", random_emotion.getId());
+        String jsonString = json.toString();
+
+        Toast.makeText(getApplicationContext(),jsonString, Toast.LENGTH_LONG).show();
+
+        /*RequestBody body = RequestBody.create(JSON, jsonString);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();*/
+
+        try {
+
+            final String requestBody = jsonString;
+
+    //            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, json, new Response.Listener<JSONObject>() {
+    //                @Override
+    //                public void onResponse(JSONObject response) {
+    //                    System.out.println(response);
+    //                }
+    //            }, new Response.ErrorListener() {
+    //                @Override
+    //                public void onErrorResponse(VolleyError error) {
+    //                    error.printStackTrace();
+    //                }
+    //            });
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        // can get more details such as response.headers
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     private void dispatchTakePictureIntent() {
@@ -67,6 +184,7 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         } catch (ActivityNotFoundException e) {
             // display error state to the user
+            Toast.makeText(getApplicationContext(),"ActivityNotFoundException", Toast.LENGTH_SHORT).show();
         }
     }
     //@SuppressLint("MissingSuperCall")
@@ -77,11 +195,19 @@ public class MainActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             img_photo.setImageBitmap(imageBitmap);
-
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            photo_byte_array = stream.toByteArray();
+            encodePhoto(imageBitmap);
         }
+    }
+
+    private void encodePhoto(Bitmap imageBitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] photo_byte_array = stream.toByteArray();
+
+//        String encoded = Base64.getEncoder().encodeToString("Hello".getBytes());
+        Base64 codec = new Base64();
+        encodedPhoto = codec.encodeBase64String(photo_byte_array);
+//        Toast.makeText(getApplicationContext(),encodedPhoto, Toast.LENGTH_LONG).show();
     }
 
 

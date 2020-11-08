20bb1c35b0ef89c6d43f9daf386df8098dc368ba
notnamenot <izabela.pachel@wp.pl>
2020-11-08 13:07:50 +0100

    photo taking

--- FERApp/app/src/main/java/pl/edu/agh/studio2/ferapp/MainActivity.java
+++ FERApp/app/src/main/java/pl/edu/agh/studio2/ferapp/MainActivity.java
@@ -2,13 +2,29 @@ package pl.edu.agh.studio2.ferapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     TextView txt_random_emotion;
     Emotion random_emotion;
+    Button btn_take_photo;
+    Button btn_send_res;
+    ImageView img_photo;
+    byte[] photo_byte_array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +35,54 @@ public class MainActivity extends AppCompatActivity {
 
         txt_random_emotion = (TextView) findViewById(R.id.txt_random_emo);
         txt_random_emotion.setText(random_emotion.name());  //String.valueOf(random_emotion))
+
+        btn_take_photo = findViewById(R.id.btn_take_photo);
+        img_photo = findViewById(R.id.img_photo_taken);
+
+        btn_send_res = findViewById(R.id.btn_send_res);
+
+        btn_take_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        btn_send_res.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                sendPhoto();
+            }
+        });
+
+
     }
+
+    private void sendPhoto() {
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+    //@SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            img_photo.setImageBitmap(imageBitmap);
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            photo_byte_array = stream.toByteArray();
+        }
+    }
+
+
 }
